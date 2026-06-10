b4645df7141e0e9271f7f7762a4c49b716a086e2
web87 <krish.t@moontechnolabs.com>
2026-06-10 16:55:02 +0530

    fix: enforce password validators on register and change-password

--- backend/apps/accounts/serializers.py
+++ backend/apps/accounts/serializers.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError as DjangoValidationError
 from rest_framework import serializers
 
 User = get_user_model()
@@ -18,6 +20,21 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ("id", "username", "email", "password", "first_name", "last_name", "phone")
 
+    def validate(self, attrs):
+        # Run Django's configured password validators (length, common-password,
+        # numeric, and similarity to user attributes) at registration time.
+        candidate = User(
+            username=attrs.get("username", ""),
+            email=attrs.get("email", ""),
+            first_name=attrs.get("first_name", ""),
+            last_name=attrs.get("last_name", ""),
+        )
+        try:
+            validate_password(attrs["password"], user=candidate)
+        except DjangoValidationError as exc:
+            raise serializers.ValidationError({"password": list(exc.messages)})
+        return attrs
+
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
 
@@ -29,4 +46,12 @@ class PasswordChangeSerializer(serializers.Serializer):
     def validate_current_password(self, value):
         if not self.context["request"].user.check_password(value):
             raise serializers.ValidationError("Current password is incorrect.")
+        return value
+
+    def validate_new_password(self, value):
+        # Enforce the same password validators used at registration/reset.
+        try:
+            validate_password(value, user=self.context["request"].user)
+        except DjangoValidationError as exc:
+            raise serializers.ValidationError(list(exc.messages))
         return value
